5e07fed6dd3c82ad7b7b5b8f9df1df00fb17bd57
Yomna-J <61107570+Yomna-J@users.noreply.github.com>
2021-06-02 00:05:48 +0300

    Created the Header in Home Page

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.topCenter,
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                child: ClipPath(
+                  clipper: Clipper(),
+                  child: Container(
+                    height: size.height * .57,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,
+                        colors: [
+                          Color(0xFFF68159),
+                          Color(0xFFF5A05B),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: size.height * 0.10,
+                child: Image.asset(
+                  'assets/images/image-1.png',
+                  height: size.height * .5,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Clipper extends CustomClipper<Path> {
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height + 30, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
